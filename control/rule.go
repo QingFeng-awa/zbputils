66970802b69748ed1014f014ac20ae70d54e5aea
QingFeng-awa <qingfeng_awa@163.com>
2025-09-25 16:56:54 +0800

    fix(control): 移除命令组的OnlyToMe限制
    移除usage命令的OnlyToMe限制，使其能在群聊中响应

--- control/rule.go
+++ control/rule.go
@@ -396,7 +396,7 @@ func init() {
 		ctx.SendChain(message.Text("已改变全局默认启用状态: " + model.Args))
 	})
 
-	zero.OnCommandGroup([]string{"用法", "usage"}, zero.OnlyToMe).SetBlock(true).SecondPriority().
+	zero.OnCommandGroup([]string{"用法", "usage"}).SetBlock(true).SecondPriority().
 		Handle(func(ctx *zero.Ctx) {
 			model := extension.CommandModel{}
 			_ = ctx.Parse(&model)
